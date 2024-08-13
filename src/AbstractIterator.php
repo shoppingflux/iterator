4e491d7c25bd39de73d4e606ac4354a00fd8cb0e
Aurelieluciani <aurelie.luciani@shopping-feed.com>
2024-08-13 10:09:10 +0200

    Update CI

--- src/AbstractIterator.php
+++ src/AbstractIterator.php
@@ -7,9 +7,6 @@ abstract class AbstractIterator implements IteratorInterface
     /** @var iterable<mixed> */
     protected iterable $items;
 
-    /**
-     * @inheritdoc
-     */
     public function toArray(): array
     {
         return iterator_to_array($this);
